85989b2a0b39af7c1b2c76305086baf2bd6a5727
Glitch (hello-webpage) <none>
2020-05-12 13:17:37 +0000

    🎾💘 Checkpoint ./sg.sh:8105878/821

---
+++ sg.sh
@@ -0,0 +1,14 @@
+echo "   "
+echo "   "
+echo "Simple Git"
+echo "Git made easier."
+echo "________________"
+echo "Version v0.1"
+echo "   "
+echo "   "
+echo ">>>>"
+x=1
+while true
+do
+  read "com"
+done
